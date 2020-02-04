bd3062ca6a37c0776bcbc9527746e981535231d4
Lucas Meireles <meireles31@msn.com>
2020-02-04 20:24:42 +0000

    splitting client to a separate file

---
+++ src/filmowclient.rs
@@ -0,0 +1,192 @@
+use reqwest;
+use select::document::Document;
+use select::predicate::Name;
+use std::thread;
+
+#[derive(Debug)]
+pub struct FilmowClient {}
+
+impl FilmowClient {
+    pub fn new() -> FilmowClient {
+        FilmowClient {}
+    }
+
+    pub fn get_all_movies_from_watchlist(&self, user: &str) -> Vec<Movie> {
+        let mut resp = vec![];
+        let mut page_num = 1;
+        loop {
+            match self
+                .get_movie_links_from_url(self.get_watchlist_url_for_page(user, page_num).as_str())
+            {
+                Ok(links) => {
+                    let mut page_movies = self.parallel_process_links(links);
+                    println!("Movies for page {}: {:?}", page_num, page_movies);
+                    resp.append(&mut page_movies);
+                    page_num += 1;
+                }
+                _ => break,
+            }
+        }
+
+        return resp;
+    }
+
+    pub fn get_all_watched_movies(&self, user: &str) -> Vec<Movie> {
+        let mut resp = vec![];
+        let mut page_num = 1;
+        loop {
+            match self
+                .get_movie_links_from_url(self.get_watched_url_for_page(user, page_num).as_str())
+            {
+                Ok(links) => {
+                    let mut page_movies = self.parallel_process_links(links);
+                    println!("Movies for page {}: {:?}", page_num, page_movies);
+                    resp.append(&mut page_movies);
+                    page_num += 1;
+                }
+                _ => break,
+            }
+        }
+        return resp;
+    }
+
+    fn get_base_url(&self) -> String {
+        "https://filmow.com".to_string()
+    }
+
+    fn get_watchlist_url_for_page(&self, user: &str, page: i32) -> String {
+        format!(
+            "https://filmow.com/usuario/{}/filmes/quero-ver/?pagina={}",
+            user, page
+        )
+    }
+
+    fn get_watched_url_for_page(&self, user: &str, page: i32) -> String {
+        format!(
+            "https://filmow.com/usuario/{}/filmes/ja-vi/?pagina={}",
+            user, page
+        )
+    }
+
+    fn get_movie_links_from_url(&self, url: &str) -> Result<Vec<String>, &str> {
+        println!("Fetching links from Page {}", url);
+        match reqwest::get(url) {
+            Ok(resp) => {
+                if resp.status() == 404 {
+                    return Err("404 page not found");
+                }
+
+                Ok(Document::from_read(resp)
+                    .unwrap()
+                    .find(Name("a"))
+                    .filter(|n| has_attr_with_name(n, "data-movie-pk"))
+                    .map(|n| n.attr("href"))
+                    .flatten()
+                    .map(|x| self.get_base_url() + &x.to_string())
+                    .collect())
+            }
+            _ => {
+                return Err("Non Ok");
+            }
+        }
+    }
+
+    fn extract_title(&self, resp: &str) -> String {
+        return Document::from(resp)
+            .find(Name("h2"))
+            .filter(|n| {
+                n.attr("class").is_some() && n.attr("class").unwrap() == "movie-original-title"
+            })
+            .map(|n| n.text())
+            .nth(0)
+            .unwrap();
+    }
+
+    fn extract_director(&self, resp: &str) -> String {
+        return Document::from(resp)
+            .find(Name("span"))
+            .filter(|n| n.attr("itemprop").is_some() && n.attr("itemprop").unwrap() == "director")
+            .map(|n| n.text().trim().to_string())
+            .nth(0)
+            .unwrap();
+    }
+
+    fn extract_year(&self, resp: &str) -> u32 {
+        return Document::from(resp)
+            .find(Name("small"))
+            .filter(|n| n.attr("class").is_some() && n.attr("class").unwrap() == "release")
+            .map(|n| n.text())
+            .nth(0)
+            .unwrap()
+            .parse::<u32>()
+            .unwrap();
+    }
+
+    fn get_movie_from_url(&self, url: &str) -> Result<Movie, &str> {
+        match reqwest::get(url) {
+            Ok(mut resp) => {
+                if resp.status() == 404 {
+                    return Err("404 page not found");
+                }
+
+                let html_body = match resp.text() {
+                    Ok(body) => body,
+                    _ => {
+                        return Err("Error when getting html body");
+                    }
+                };
+
+                let title = self.extract_title(html_body.as_str());
+                let director = self.extract_director(html_body.as_str());
+                let year = self.extract_year(html_body.as_str());
+
+                return Ok(Movie {
+                    title: title,
+                    director: director,
+                    year: year,
+                });
+            }
+            _ => {
+                return Err("Non Ok");
+            }
+        }
+    }
+
+    fn parallel_process_links(&self, links: Vec<String>) -> Vec<Movie> {
+        let mut children = vec![];
+        for link in links {
+            children.push(thread::spawn(move || -> Movie {
+                FilmowClient::new().get_movie_from_url(&link).unwrap()
+            }));
+        }
+
+        let mut movies = vec![];
+        for child in children {
+            let movie = child.join().unwrap();
+            movies.push(movie);
+        }
+
+        return movies;
+    }
+}
+
+#[derive(Debug)]
+pub struct Movie {
+    title: String,
+    director: String,
+    year: u32,
+}
+
+impl Movie {
+    pub fn to_csvable_array(&self) -> Vec<String> {
+        return vec![
+            self.title.clone(),
+            self.director.clone(),
+            self.year.to_string(),
+        ];
+    }
+}
+
+fn has_attr_with_name(node: &select::node::Node, name: &str) -> bool {
+    node.attr(name).is_some()
+}
