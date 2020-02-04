7f080f1fe39406cdfca10a07c9d6e88ed0e8c667
Lucas Meireles <meireles31@msn.com>
2020-02-04 20:39:38 +0000

    Add error handling for csv saving

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,46 @@ mod filmowclient;
 use filmowclient::FilmowClient;
 use filmowclient::Movie;
 
-fn save_movies_to_csv(movies: Vec<Movie>, file_name: &str) {
+fn save_movies_to_csv(movies: Vec<Movie>, file_name: &str) -> Result<(), String> {
     let mut wrt = Writer::from_path(file_name).unwrap();
-    wrt.write_record(&["Title", "Directors", "Year"]);
-    for movie in movies.iter() {
-        wrt.write_record(movie.to_csvable_array());
+    match wrt.write_record(&["Title", "Directors", "Year"]) {
+        Err(e) => {
+            return Err(format!(
+                "Error when adding header to Csv file {}. {:?}",
+                file_name, e
+            ))
+        }
+        _ => {}
     }
-    wrt.flush();
+    for movie in movies.iter() {
+        match wrt.write_record(movie.to_csvable_array()) {
+            Err(e) => {
+                return Err(format!(
+                    "Error when adding entry to Csv file {}. Entry: {:?}, Error:{:?}",
+                    file_name, movie, e
+                ))
+            }
+            _ => {}
+        }
+    }
+    match wrt.flush() {
+        Err(e) => return Err(format!("Error when flushing file {}. {:?}", file_name, e)),
+        _ => {}
+    };
+    Ok(())
 }
 
 fn main() {
     let user = env::args().nth(1).unwrap();
     let client = FilmowClient::new();
     let watchlist_movies = client.get_all_movies_from_watchlist(user.as_str());
-    save_movies_to_csv(watchlist_movies, "watchlist.csv");
+    match save_movies_to_csv(watchlist_movies, "watchlist.csv") {
+        Err(e) => return println!("Error when saving watchlist: {:?}", e),
+        _ => {}
+    }
     let watched_movies = client.get_all_watched_movies(user.as_str());
-    save_movies_to_csv(watched_movies, "watched.csv");
+    match save_movies_to_csv(watched_movies, "watched.csv") {
+        Err(e) => return println!("Error when saving watched movies: {:?}", e),
+        _ => {}
+    }
 }
