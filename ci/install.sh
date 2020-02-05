9b2a6827c0458e92446b48e57f2aa0cb22c2f93e
Lucas Meireles <meireles31@msn.com>
2020-02-05 10:48:33 +0000

    rustup correclty for nightly on install.sh

--- ci/install.sh
+++ ci/install.sh
@@ -10,23 +10,12 @@ main() {
         sort=gsort  # for `sort --sort-version`, from brew's coreutils.
     fi
 
-    # Builds for iOS are done on OSX, but require the specific target to be
-    # installed.
     case $TARGET in
-        aarch64-apple-ios)
-            rustup target install aarch64-apple-ios
+        nightly-x86_64-unknown-linux-gnu)
+            rustup component add rustc --toolchain nightly-x86_64-unknown-linux-gnu
             ;;
-        armv7-apple-ios)
-            rustup target install armv7-apple-ios
-            ;;
-        armv7s-apple-ios)
-            rustup target install armv7s-apple-ios
-            ;;
-        i386-apple-ios)
-            rustup target install i386-apple-ios
-            ;;
-        x86_64-apple-ios)
-            rustup target install x86_64-apple-ios
+        nightly-x86_64-apple-darwin)
+            rustup component add rustc --toolchain nightly-x86_64-apple-darwin
             ;;
     esac
 
