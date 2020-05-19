87a7a0c13982056d34c6aa61cf48ced92c3910c6
Lucas Meireles <meireles31@msn.com>
2020-05-19 19:24:27 +0100

    remove lto compile arg.

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -17,7 +17,7 @@ main() {
 
     test -f Cargo.lock || cargo generate-lockfile
 
-    cross rustc --bin filmow2letterboxd --target $TARGET --release -- -C lto
+    cross rustc --bin filmow2letterboxd --target $TARGET --release
 
     cp target/$TARGET/release/filmow2letterboxd $stage/
 
