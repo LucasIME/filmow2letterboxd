c8a9bffe7f21870f7245e6f5838c7e7538db0305
Lucas Meireles <meireles31@msn.com>
2020-06-18 18:22:25 +0100

    more clippy fix

--- src/csvwriter.rs
+++ src/csvwriter.rs
@@ -8,30 +8,25 @@ impl CsvWriter {
     pub fn save_movies_to_csv(movies: Vec<Movie>, file_name: &str) -> Result<(), String> {
         let mut wrt = Writer::from_path(file_name)
             .unwrap_or_else(|_| panic!("Could not create CSV Writer for file {}", file_name));
-        match wrt.write_record(&Movie::csv_titles()) {
-            Err(e) => {
-                return Err(format!(
-                    "Error when adding header to Csv file {}. {:?}",
-                    file_name, e
-                ))
-            }
-            _ => {}
+        if let Err(e) = wrt.write_record(&Movie::csv_titles()) {
+            return Err(format!(
+                "Error when adding header to Csv file {}. {:?}",
+                file_name, e
+            ));
         }
         for movie in movies.iter() {
-            match wrt.write_record(movie.to_csvable_array()) {
-                Err(e) => {
-                    return Err(format!(
-                        "Error when adding entry to Csv file {}. Entry: {:?}, Error:{:?}",
-                        file_name, movie, e
-                    ))
-                }
-                _ => {}
+            if let Err(e) = wrt.write_record(movie.to_csvable_array()) {
+                return Err(format!(
+                    "Error when adding entry to Csv file {}. Entry: {:?}, Error:{:?}",
+                    file_name, movie, e
+                ));
             }
         }
-        match wrt.flush() {
-            Err(e) => return Err(format!("Error when flushing file {}. {:?}", file_name, e)),
-            _ => {}
-        };
+
+        if let Err(e) = wrt.flush() {
+            return Err(format!("Error when flushing file {}. {:?}", file_name, e));
+        }
+
         Ok(())
     }
 }
